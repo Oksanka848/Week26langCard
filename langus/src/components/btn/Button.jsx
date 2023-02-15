74b040930c9de357856015fc6abdb12fb45305d0
Oksanka848 <okmitka@mail.ru>
2023-02-15 06:11:50 +0000

    btn

---
+++ langus/src/components/btn/Button.jsx
@@ -0,0 +1,10 @@
+import style from './btn/button.module.scss';
+
+export default function ButtonS() {
+  return (
+    <>{
+      <div className={`${style.buttonS}`}><span className={`${style.glowingtxt}`}>C<span className={`${style.faultyletter}`}>L</span>ICK</span></div>
+
+    }</>
+  )
+}
